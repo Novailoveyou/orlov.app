d21b2045bd5b4a76257d70c9a9ed2148e1c907e4
Ilia Orlov <novailoveyoux@gmail.com>
2025-01-20 21:57:09 +0300

    feat: better styles

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
-import './index.scss'
+import './index.css'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   darkMode: ['class'],
   content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './hooks/**/*.{js,ts,jsx,tsx,mdx}',
     './lib/**/*.{js,ts,jsx,tsx,mdx}',
