ad163f74b9944939ea4347b619c0cf577335687c
Ilia Orlov <novailoveyoux@gmail.com>
2025-01-21 01:38:46 +0300

    feat: run format

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -4,7 +4,7 @@ import tailwindconfig, { TailwindConfig } from '@repo/ui/tailwind.config'
 
 const config = {
   ...tailwindconfig,
-  presets: [tailwindconfig],
+  presets: [tailwindconfig]
 } as const satisfies TailwindConfig
 
 export default config
