c8e98d74b684bbd0f89326a5d4d1e8bf02355e07
Ilia Orlov <novailoveyoux@gmail.com>
2025-03-05 14:36:20 +0300

    feat: better structure & faster perfomance

--- apps/snake/src/main.ts
+++ apps/snake/src/main.ts
@@ -17,191 +17,225 @@ import './style.css'
 //   }
 // })()
 
-const getRandomInt = (min: number, max: number) => {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
+type Cell = [number, number]
 
-const Snake = (() => {
-  type Cell = [number, number]
+type ElementId = Parameters<typeof document.getElementById>['0']
 
-  const SIZE = 50
-  // const SPEED = 1000 / 60
-  const SPEED = 43
-  const HEAD: Cell = [0, 0]
-  const SNAKE = [/* TAIL */ HEAD]
-  const TAIL: Record<`${Cell[number]}-${Cell[number]}`, Cell | null> = {}
-  const FOOD: Record<`${Cell[number]}-${Cell[number]}`, Cell | null> = {}
+const Utils = (() => {
+  const randomNumber = (min: number, max: number): number => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
 
-  // let width = window.innerWidth
-  // let height = window.innerHeight
-  let direction: 'up' | 'right' | 'down' | 'left' = 'right'
-  let pause = false
+  return { randomNumber }
+})()
 
-  const getHead = () => SNAKE[SNAKE.length - 1]
+// const Screen = (() => {
+//   let width = window.innerWidth
+//   let height = window.innerHeight
+//   const handleResize = (_: UIEvent) => {
+//     width = window.innerWidth
+//     height = window.innerHeight
+//   }
+//   window.addEventListener('resize', handleResize)
+//   window.removeEventListener('resize', handleResize)
+// })()
 
-  const getCell = (cell: Cell | undefined) =>
-    !cell ? null : document.getElementById(`${cell[0]}-${cell[1]}`)
+const Field = (() => {
+  const field = document.createElement('div')
+  field.classList.add('field')
 
-  // const handleResize = (_: UIEvent) => {
-  //   width = window.innerWidth
-  //   height = window.innerHeight
-  // }
+  const init = (size: number) => {
+    if (size === field.childNodes.length) return field
 
-  const createField = () => {
-    const field = document.createElement('div')
-    field.classList.add('field')
+    const createColumn = (_: unknown, x: number) => {
+      const createRow = (_: unknown, y: number) => {
+        const cell = document.createElement('div')
+        cell.setAttribute('id', `${x}-${y}`)
+        cell.classList.add('cell')
+
+        return cell
+      }
+
+      const rows = Array.from({ length: size }, createRow)
+
+      const column = document.createElement('div')
+      column.classList.add('column')
+
+      column.append(...rows)
+
+      return column
+    }
+
+    const columns = Array.from({ length: size }, createColumn)
+
+    field.append(...columns)
 
     return field
   }
 
-  const createColumn = (_: unknown, x: number) => {
-    const createRow = (_: unknown, y: number) => {
-      const cell = document.createElement('div')
-      cell.setAttribute('id', `${x}-${y}`)
-      cell.classList.add('cell')
+  const cell = (cell: Cell | undefined) =>
+    !cell ? null : document.getElementById(`${cell[0]}-${cell[1]}`)
 
-      return cell
+  return { init, cell }
+})()
+
+const Snake = ((Utils, Field) => {
+  const SIZE = 50
+  const SPEED = 43
+  const HEAD: Cell = [0, 0]
+  const SNAKE = [/* TAIL */ HEAD]
+
+  let direction: 'up' | 'right' | 'down' | 'left' = 'right'
+  let isPause = false
+
+  const forward = () => {
+    const [x, y] = SNAKE[SNAKE.length - 1]
+    let head: Cell
+
+    switch (direction) {
+      case 'up':
+        if (y - 1 < 0) {
+          head = [x, SIZE - 1]
+          break
+        }
+        head = [x, y - 1]
+        break
+      case 'right':
+        if (x + 1 > SIZE - 1) {
+          head = [0, y]
+          break
+        }
+        head = [x + 1, y]
+        break
+      case 'down':
+        if (y + 1 > SIZE - 1) {
+          head = [x, 0]
+          break
+        }
+        head = [x, y + 1]
+        break
+      case 'left':
+        if (x - 1 < 0) {
+          head = [SIZE - 1, y]
+          break
+        }
+        head = [x - 1, y]
+        break
     }
 
-    const rows = Array.from({ length: SIZE }, createRow)
-
-    const column = document.createElement('div')
-    column.classList.add('column')
-
-    column.append(...rows)
-
-    return column
+    return head
   }
 
-  const field = createField()
+  const move = () => {
+    const head = forward()
 
-  const columns = Array.from({ length: SIZE }, createColumn)
+    const _headCell = Field.cell(SNAKE[SNAKE.length - 1])!
+    const headCell = Field.cell(head)!
 
-  field.append(...columns)
+    if (headCell.classList.contains('food')) {
+      headCell.classList.remove('food')
+    } else {
+      const tail = SNAKE.shift()
+      Field.cell(tail)?.classList.remove('player')
+    }
+
+    _headCell.classList.remove('head')
+    _headCell.innerHTML = ''
+
+    if (headCell.classList.contains('player')) {
+      pause()
+      alert(`You ate yourself. Your tail grew to the size of ${SNAKE.length}`)
+      window.location.reload()
+    } else {
+      SNAKE.push(head)
+
+      headCell.classList.add('player', 'head')
+      headCell.innerHTML = `<span>${SNAKE.length - 1}<span>`
+    }
+  }
+
+  const feed = () => {
+    const x = Utils.randomNumber(0, SIZE)
+    const y = Utils.randomNumber(0, SIZE)
+
+    Field.cell([x, y])?.classList.add('food')
+  }
+
+  const pause = () => {
+    isPause = true
+    clearInterval(Number(sessionStorage.getItem('moveInterval')))
+    clearInterval(Number(sessionStorage.getItem('foodInterval')))
+
+    return true
+  }
+
+  const play = () => {
+    isPause = false
+
+    const moveInterval = setInterval(move, SPEED)
+    sessionStorage.setItem('moveInterval', `${moveInterval}`)
+
+    const foodInterval = setInterval(feed, SPEED * 10)
+    sessionStorage.setItem('foodInterval', `${foodInterval}`)
+
+    return pause
+  }
 
   const handleKeydown = (e: KeyboardEvent) => {
     switch (e.key) {
       case 'ArrowUp':
       case 'w':
+        if (isPause) break
         direction = 'up'
         break
       case 'ArrowRight':
       case 'd':
+        if (isPause) break
         direction = 'right'
         break
       case 'ArrowDown':
       case 's':
+        if (isPause) break
         direction = 'down'
         break
       case 'ArrowLeft':
       case 'a':
+        if (isPause) break
         direction = 'left'
         break
+      case ' ':
+        isPause ? play() : pause()
+        break
     }
   }
 
-  const move = () => {
-    if (pause) return
-    const [_x, _y] = getHead()
+  const init = (id: ElementId) => {
+    const field = Field.init(SIZE)
 
-    let newHead: Cell = [_x, _y]
+    document.getElementById(id)?.appendChild(field)
 
-    switch (direction) {
-      case 'up':
-        if (_y - 1 < 0) {
-          newHead = [_x, SIZE - 1]
-          break
-        }
-        newHead = [_x, _y - 1]
-        break
-      case 'right':
-        if (_x + 1 > SIZE - 1) {
-          newHead = [0, _y]
-          break
-        }
-        newHead = [_x + 1, _y]
-        break
-      case 'down':
-        if (_y + 1 > SIZE - 1) {
-          newHead = [_x, 0]
-          break
-        }
-        newHead = [_x, _y + 1]
-        break
-      case 'left':
-        if (_x - 1 < 0) {
-          newHead = [SIZE - 1, _y]
-          break
-        }
-        newHead = [_x - 1, _y]
-        break
-    }
-
-    if (FOOD[`${newHead[0]}-${newHead[1]}`]) {
-      FOOD[`${newHead[0]}-${newHead[1]}`] = null
-      getCell(newHead)?.classList.remove('food')
-    } else {
-      const tail = SNAKE.shift()
-      if (tail) TAIL[`${tail[0]}-${tail[1]}`] = null
-      getCell(tail)?.classList.remove('player')
-    }
-
-    if (TAIL[`${newHead[0]}-${newHead[1]}`]) {
-      pause = true
-      alert('lost')
-      window.location.reload()
-    } else {
-      SNAKE.push(newHead)
-      if (SNAKE.length > 1) TAIL[`${_x}-${_y}`] = [_x, _y]
-    }
-
-    getCell([_x, _y])?.classList.remove('head')
-    getCell([_x, _y])!.innerHTML = ''
-    getCell(newHead)?.classList.add('player', 'head')
-    getCell(newHead)!.innerHTML = `<span>${SNAKE.length}<span>`
-  }
-
-  const feed = () => {
-    if (pause) return
-    const x = getRandomInt(0, SIZE)
-    const y = getRandomInt(0, SIZE)
-
-    getCell([x, y])?.classList.add('food')
-
-    FOOD[`${x}-${y}`] = [x, y]
-  }
-
-  const init = () => {
-    // window.addEventListener('resize', handleResize)
+    play()
 
     window.addEventListener('keydown', handleKeydown)
 
-    document.getElementById('app')?.appendChild(field)
-
-    const moveInterval = setInterval(move, SPEED)
-    const foodInterval = setInterval(feed, SPEED * 10)
-
     return () => {
-      // window.removeEventListener('resize', handleResize)
+      document.getElementById(id)?.removeChild(field)
+
+      pause()
 
       window.removeEventListener('keydown', handleKeydown)
-
-      document.getElementById('app')?.removeChild(field)
-
-      clearInterval(moveInterval)
-      clearInterval(foodInterval)
     }
   }
 
   return { init }
-})()
+})(Utils, Field)
 
 const App = (() => {
-  const init = () => Snake.init()
+  const init = (id: ElementId) => Snake.init(id)
 
   return { init }
 })()
 
-App.init()
+App.init('app')
